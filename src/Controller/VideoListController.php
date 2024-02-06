990302dcab8bcba0571279a4cef174e4cea57c7b
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-02-06 17:12:52 +0000

    Views

--- src/Controller/VideoListController.php
+++ src/Controller/VideoListController.php
@@ -17,27 +17,7 @@ class VideoListController implements Controller
     public function processaRequisicao(): void
     {
         $videoList = $this->videoRepository->allVideo();
-        require_once __DIR__ . '/../../inicio-html.php'; ?>
+        require_once __DIR__ . '/../../views/video-list.php';
 
-
-        <ul class="videos__container">
-        <?php foreach ($videoList as $video): ?>
-            <li class="videos__item">
-                <iframe width="100%" height="72%" src="<?= $video->url; ?>"
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <h3><?= $video->title; ?></h3>
-                </div>
-                    <div class="acoes-video">
-                        <a href="/editar-video?id=<?= $video->id; ?>">Editar</a>
-                        <a href="/remover-video?id=<?= $video->id; ?>">Excluir</a>
-                    </div>
-
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php require_once __DIR__ . '/../../fim-html.php';
     }
 }
