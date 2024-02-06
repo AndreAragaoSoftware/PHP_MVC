f447ee9503a145ed276b3fd55978a4c9ca19e3c0
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-02-06 17:41:33 +0000

    Estabelecendo rotas

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'GET|/' => \Andre\Mvc\Controller\VideoListController::class,
+    'GET|/novo-video' => \Andre\Mvc\Controller\VideoFormController::class,
+    'POST|/novo-video' => \Andre\Mvc\Controller\VideoNewController::class,
+    'GET|/editar-video' => \Andre\Mvc\Controller\VideoFormController::class,
+    'POST|/editar-video' => \Andre\Mvc\Controller\VideoEditController::class,
+    'GET|/remover-video' => \Andre\Mvc\Controller\VideRemoveController::class
+];
--- public/index.php
+++ public/index.php
@@ -19,26 +19,20 @@ $videoRepository = new VideoRepository($pdo);
 
 /** @var Andre\Mvc\Controller\Controller $controller */
 
-if(!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/'){
-    $controller = new VideoListController($videoRepository);
-} elseif (($_SERVER['PATH_INFO'] === '/novo-video')){
-    if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        $controller = new VideoFormController($videoRepository);
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $controller = new VideoNewController($videoRepository);
-    }
-} elseif (($_SERVER['PATH_INFO'] === '/editar-video')){
-    if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        $controller = new VideoFormController($videoRepository);
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $controller = new VideoEditController($videoRepository);
-    }
-} elseif (($_SERVER['PATH_INFO'] === '/remover-video')) {
-    $controller = new VideRemoveController($videoRepository);
-}else {
-    // Se não encontrar a pagina
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+// Se não não achar vai ser /
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+if (array_key_exists($key, $routes)) {
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+
+    $controller = new $controllerClass($videoRepository);
+} else {
     $controller = new Error404Controller();
 }
-
 /** @var Controller $controller */
 $controller->processaRequisicao();
