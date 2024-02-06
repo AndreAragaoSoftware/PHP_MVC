429353396585927a3d15da2dd87e48a8556596f0
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-02-06 11:49:26 +0000

    PAsta public

--- public/index.php
+++ public/index.php
@@ -4,19 +4,22 @@
 declare(strict_types=1);
 
 if(!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/'){
-    require_once 'listagem-cursos.php';
+    require_once __DIR__ . '/../listagem-cursos.php';
 } elseif (($_SERVER['PATH_INFO'] === '/novo-video')){
     if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once 'formulario.php';
+        require_once __DIR__ . '/../formulario.php';
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once 'novo-video.php';
+        require_once __DIR__ . '/../novo-video.php';
     }
 } elseif (($_SERVER['PATH_INFO'] === '/editar-video')){
     if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once 'formulario.php';
+        require_once __DIR__ . '/../formulario.php';
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once 'editar-video.php';
+        require_once __DIR__ . '/../editar-video.php';
     }
 }elseif (($_SERVER['PATH_INFO'] === '/remover-video')) {
-    require_once 'remover-video.php';
+    require_once __DIR__ . '/../remover-video.php';
+}else {
+    // Se não encontrar a pagina
+    http_response_code(404);
 }
